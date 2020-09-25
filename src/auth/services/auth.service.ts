2f9ae988cd0bc672ca6a6c241913dc244e77e8b9
Charilaou <charilaou.c@gmail.com>
2020-09-25 16:49:43 +0300

    install bcrypt

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable, from } from 'rxjs';
+import{User} from 'src/user/models/user.interface'
+const bcrypt=require('bcrypt');
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private readonly jwtService: JwtService){
+
+    }
+
+    generateJWT(user: User): Observable <string>{
+        return from(this.jwtService.signAsync({user}));
+    }
+
+    hashPassword(password: string): Observable <string>{
+        return from<string>(bcrypt.hash(password, 12));
+    }
+
+    comparePasswords(): Observable <any>{
+
+    }
+}
