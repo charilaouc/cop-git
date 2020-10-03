6e23799d3b65e846c7a093095da564ae95b8ff8c
charilaouc <charilaou.c@gmail.com>
2020-10-03 15:16:05 +0000

    start setup react app

---
+++ cosmosplatform/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdRdSF0wAa7O4KciOvS_UpHOPvZwlbZOw",
+  authDomain: "cosmosplatform-5ea79.firebaseapp.com",
+  databaseURL: "https://cosmosplatform-5ea79.firebaseio.com",
+  projectId: "cosmosplatform-5ea79",
+  storageBucket: "cosmosplatform-5ea79.appspot.com",
+  messagingSenderId: "709729139126",
+  appId: "1:709729139126:web:beb252990d999f14d42917",
+  measurementId: "G-4RZ41GY954"
+};
