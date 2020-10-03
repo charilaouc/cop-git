4d755d1963153cc9a3c7f78d311fdf2ea6b1e994
charilaouc <charilaou.c@gmail.com>
2020-10-03 16:35:38 +0000

    header

---
+++ cosmosplatform/src/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import'./Header.css'
+
+function Header() {
+    return (
+        <div className  = 'header'>
+            <img className="header__logo" src="">
+            </img>
+
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+                {
+                //logo
+                }
+
+            </div>
+            <div className="header__nav">
+                <div className='header__option'>
+
+                </div>
+                <div className='header__option'>
+                    
+                </div>
+                <div className='header__option'>
+                    
+                </div>
+                <div className='header__option'>
+                    
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
