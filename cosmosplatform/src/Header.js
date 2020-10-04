419e88fee8d4b9418e59f7fb7953fa2bf08c2fc3
charilaouc <charilaou.c@gmail.com>
2020-10-04 21:38:19 +0000

    data

--- cosmosplatform/src/Header.js
+++ cosmosplatform/src/Header.js
@@ -5,7 +5,7 @@ import SearchIcon from "@material-ui/icons/Search";
 function Header() {
     return (
         <div className  = 'header'>
-            <img className="header__logo" src="https://github.com/charilaouc/cop-git/blob/800fc1700393f7ef8cac2d27a74baf6a48391f2e/cosmosplatform/src/images/logo.png">
+            <img className="header__logo" src="https://github.com/charilaouc/cop-git/blob/master/cosmosplatform/src/images/social.jpg">
             </img>
 
             <div className="header__search">
