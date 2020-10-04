6a02d9b6af1328bbbd206f50edb7179acfee887a
charilaouc <charilaou.c@gmail.com>
2020-10-04 20:20:13 +0000

    searchicon and material ui

--- cosmosplatform/src/Header.js
+++ cosmosplatform/src/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import'./Header.css'
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
     return (
@@ -9,6 +10,7 @@ function Header() {
 
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
+                <SearchIcon className="header__searchIcon"/>
                 {
                 //logo
                 }
