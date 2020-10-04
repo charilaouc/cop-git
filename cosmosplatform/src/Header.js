19d923658a49ed9ddc4fc6fcf5371f46b8126b49
charilaouc <charilaou.c@gmail.com>
2020-10-04 19:49:16 +0000

    header options plus npm start

--- cosmosplatform/src/Header.js
+++ cosmosplatform/src/Header.js
@@ -16,16 +16,20 @@ function Header() {
             </div>
             <div className="header__nav">
                 <div className='header__option'>
-
+                    <span className='header__optionLineOne'>Hello User</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
                 </div>
                 <div className='header__option'>
-                    
+                    <span className='header__optionLineOne'>Hello User</span>
+                    <span className='header__optionLineTwo'>Sign In</span>                    
                 </div>
                 <div className='header__option'>
-                    
+                    <span className='header__optionLineOne'>Hello User</span>
+                    <span className='header__optionLineTwo'>Sign In</span>                    
                 </div>
                 <div className='header__option'>
-                    
+                    <span className='header__optionLineOne'>Hello User</span>
+                    <span className='header__optionLineTwo'>Sign In</span>                    
                 </div>
             </div>
 
