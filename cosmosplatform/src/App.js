25abb7f2226056ac46648d195937c0a8b540f319
charilaouc <charilaou.c@gmail.com>
2020-10-04 20:10:44 +0000

    css details

--- cosmosplatform/src/App.js
+++ cosmosplatform/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>hello world</h1>
+
+      <Header/>
+      {
+          //Home
+      }
     </div>
   );
 }
--- cosmosplatform/src/Header.js
+++ cosmosplatform/src/Header.js
@@ -4,7 +4,7 @@ import'./Header.css'
 function Header() {
     return (
         <div className  = 'header'>
-            <img className="header__logo" src="">
+            <img className="header__logo" src="./logo.png">
             </img>
 
             <div className="header__search">
@@ -20,16 +20,16 @@ function Header() {
                     <span className='header__optionLineTwo'>Sign In</span>
                 </div>
                 <div className='header__option'>
-                    <span className='header__optionLineOne'>Hello User</span>
-                    <span className='header__optionLineTwo'>Sign In</span>                    
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>& Orders</span>                    
                 </div>
                 <div className='header__option'>
-                    <span className='header__optionLineOne'>Hello User</span>
-                    <span className='header__optionLineTwo'>Sign In</span>                    
+                    <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineTwo'>Prime</span>                    
                 </div>
                 <div className='header__option'>
-                    <span className='header__optionLineOne'>Hello User</span>
-                    <span className='header__optionLineTwo'>Sign In</span>                    
+                    <span className='header__optionLineOne'>sample</span>
+                    <span className='header__optionLineTwo'>sample</span>                    
                 </div>
             </div>
 
